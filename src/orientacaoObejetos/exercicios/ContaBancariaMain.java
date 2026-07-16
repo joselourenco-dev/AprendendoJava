7370194e31940fd355260afecbdce18b4976d380
José Lourenco <josern1771@gmail.com>
2026-07-15 23:05:03 -0300

    Teste de valores

--- src/orientacaoObejetos/exercicios/ContaBancariaMain.java
+++ src/orientacaoObejetos/exercicios/ContaBancariaMain.java
@@ -3,7 +3,7 @@ package orientacaoObejetos.exercicios;
 public class ContaBancariaMain {
     public static void main(String[] args){
         ContaBancaria contaBancaria = new ContaBancaria("Alex", 1500);
-        contaBancaria.depositar(0123);
+        contaBancaria.depositar(23);
         System.out.println("Seu saldo atual é: " + contaBancaria.getSaldo());
 
     }
