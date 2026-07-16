aa7eaaf894f17d9b8d4a154489003d0c1197331d
José Lourenco <josern1771@gmail.com>
2026-07-15 22:00:37 -0300

    Refatoramento usando construtor

--- src/orientacaoObejetos/exercicios/Carro.java
+++ src/orientacaoObejetos/exercicios/Carro.java
@@ -4,6 +4,11 @@ public class Carro {
     String modelo;
     int ano;
 
+    Carro(String modelo, int ano){
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
     void exirbirInfo(){
         System.out.println("Modelo: " + modelo + "\nAno: " + ano);
     }
--- src/orientacaoObejetos/exercicios/CarroMain.java
+++ src/orientacaoObejetos/exercicios/CarroMain.java
@@ -2,13 +2,11 @@ package orientacaoObejetos.exercicios;
 
 public class CarroMain {
     public static void main(String[] args){
-        Carro carro1 = new Carro();
-        carro1.modelo = "Fiat uno";
-        carro1.ano = 2015;
+        Carro carro1 = new Carro("Fiat uno", 2015);
 
-        Carro carro2 = new Carro();
-        carro2.modelo = "Celta";
-        carro2.ano = 2022;
+
+        Carro carro2 = new Carro("Celta", 2022);
+
 
         carro1.exirbirInfo();
         carro2.exirbirInfo();
