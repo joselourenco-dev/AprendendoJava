b16ac27b2cfa03a5e712d7e08b09dcce9d2fc5cf
José Lourenco <josern1771@gmail.com>
2026-07-15 21:55:01 -0300

    Refatoramento usando construtor

--- src/orientacaoObejetos/exercicios/Produto.java
+++ src/orientacaoObejetos/exercicios/Produto.java
@@ -4,6 +4,12 @@ public class Produto {
     String nome;
     double preco;
 
+    Produto(String nome, double preco){
+        this.nome = nome;
+        this.preco = preco;
+        
+    }
+
     void aplicarDescontoPercentual(double percentual) {
         double precoComDesconto = preco - (preco * (percentual / 100));
         System.out.println(nome + " com " + percentual + "% de desconto: " + precoComDesconto);
--- src/orientacaoObejetos/exercicios/ProdutoMain.java
+++ src/orientacaoObejetos/exercicios/ProdutoMain.java
@@ -2,9 +2,7 @@ package orientacaoObejetos.exercicios;
 
 public class ProdutoMain {
     public static void main(String[] args){
-        Produto produto = new Produto();
-        produto.nome = "Geladeira";
-        produto.preco = 1800;
+        Produto produto = new Produto("Geladeira", 1800);
 
         produto.aplicarDescontoPercentual(15);
         produto.aplicarDescontoPercentual(50);
