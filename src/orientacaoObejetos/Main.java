68494566b62a16a7b9acb6db0577fcfe4b92218a
José Lourenco <josern1771@gmail.com>
2026-07-15 21:19:52 -0300

    Introdução a orientação a objetos

---
+++ src/orientacaoObejetos/Main.java
@@ -0,0 +1,17 @@
+package orientacaoObejetos;
+
+public class Main {
+    public static void main(String[] args){
+        Pessoa pessoa1 = new Pessoa();
+        pessoa1.nome = "Alana";
+        pessoa1.idade = 15;
+
+        Pessoa pessoa2 = new Pessoa();
+        pessoa2.nome = "João";
+        pessoa2.idade = 58;
+
+        pessoa1.apresentar();
+        pessoa2.apresentar();
+
+    }
+}
---
+++ src/orientacaoObejetos/Pessoa.java
@@ -0,0 +1,10 @@
+package orientacaoObejetos;
+
+public class Pessoa {
+    String nome;
+    int idade;
+
+    void apresentar(){
+        System.out.println("Olá me chamo " + nome + " e tenho " + idade + " anos");
+    }
+}
