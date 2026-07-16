f9da29a9604f8a880291083f1572de7c983ed672
José Lourenco <josern1771@gmail.com>
2026-07-15 23:04:23 -0300

    Correção da validação de deposito

--- src/orientacaoObejetos/exercicios/ContaBancaria.java
+++ src/orientacaoObejetos/exercicios/ContaBancaria.java
@@ -2,21 +2,21 @@ package orientacaoObejetos.exercicios;
 
 public class ContaBancaria {
     private String titular;
-    private  double saldo;
+    private double saldo;
 
 
-
-    ContaBancaria(String titular, double saldo){
+    ContaBancaria(String titular, double saldo) {
         this.titular = titular;
         this.saldo = saldo;
     }
 
-    double depositar(double valor){
-        if (valor <= 0){
+    double depositar(double valor) {
+        if (valor <= 0) {
             System.out.println("Valor inválido");
+            return saldo;
         }
 
-        saldo = saldo+valor;
+        saldo = saldo + valor;
         return saldo;
     }
 
@@ -24,15 +24,7 @@ public class ContaBancaria {
         return saldo;
     }
 
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
-    }
-
     public String getTitular() {
         return titular;
-    }
-
-    public void setTitular(String titular) {
-        this.titular = titular;
     }
 }
