93257457c56316ece8f35e8647774c34c51baadd
José Lourenco <josern1771@gmail.com>
2026-07-15 22:51:57 -0300

    Refatoração - add getters e setters

--- src/orientacaoObejetos/exercicios/ContaBancaria.java
+++ src/orientacaoObejetos/exercicios/ContaBancaria.java
@@ -1,8 +1,10 @@
 package orientacaoObejetos.exercicios;
 
 public class ContaBancaria {
-    String titular;
-    double saldo;
+    private String titular;
+    private  double saldo;
+
+
 
     ContaBancaria(String titular, double saldo){
         this.titular = titular;
@@ -10,8 +12,27 @@ public class ContaBancaria {
     }
 
     double depositar(double valor){
+        if (valor <= 0){
+            System.out.println("Valor inválido");
+        }
+
         saldo = saldo+valor;
         return saldo;
     }
 
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
 }
--- src/orientacaoObejetos/exercicios/ContaBancariaMain.java
+++ src/orientacaoObejetos/exercicios/ContaBancariaMain.java
@@ -3,8 +3,9 @@ package orientacaoObejetos.exercicios;
 public class ContaBancariaMain {
     public static void main(String[] args){
         ContaBancaria contaBancaria = new ContaBancaria("Alex", 1500);
+        contaBancaria.depositar(0123);
+        System.out.println("Seu saldo atual é: " + contaBancaria.getSaldo());
 
-        System.out.println("Seu saldo atual é: " + contaBancaria.depositar(500));
     }
 }
 
