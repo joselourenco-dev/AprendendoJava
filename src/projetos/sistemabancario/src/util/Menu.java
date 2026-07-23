1fb7c6f9f3f30115ce81f5ee23fbeed309c2c251
José Lourenco <josern1771@gmail.com>
2026-07-23 14:52:34 -0300

    Esqueleto do Menu

--- src/projetos/sistemabancario/src/util/Menu.java
+++ src/projetos/sistemabancario/src/util/Menu.java
@@ -1,4 +1,11 @@
 package projetos.sistemabancario.src.util;
 
 public class Menu {
+    public void menu(){
+        System.out.println("=========================");
+        System.out.println("1 - Criar conta");
+        System.out.println("2 - Listar contas");
+        System.out.println("0 - Sair do menu");
+        System.out.println("=========================");
+    }
 }
