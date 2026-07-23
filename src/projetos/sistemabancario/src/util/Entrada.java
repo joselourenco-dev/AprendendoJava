f2acd519da9d5284b9c1210e5b1aae7d8f960314
José Lourenco <josern1771@gmail.com>
2026-07-23 14:52:14 -0300

    sistemda de entrada de dados

--- src/projetos/sistemabancario/src/util/Entrada.java
+++ src/projetos/sistemabancario/src/util/Entrada.java
@@ -1,4 +1,32 @@
 package projetos.sistemabancario.src.util;
 
+import java.util.Scanner;
+
 public class Entrada {
+    private Scanner scanner;
+
+   public Entrada(Scanner scanner){
+       this.scanner = scanner;
+   }
+
+   public  String lerTexto (String mensagem){
+
+       mensagem = scanner.nextLine();
+       return mensagem;
+   }
+
+   public int lerInteiro(int mensagem){
+       // imprime a mensagem, lê um int (tratando o problema do buffer, se precisar de nextLine depois)
+       mensagem  = scanner.nextInt();
+       scanner.nextLine();
+       return mensagem;
+   }
+
+   public double lerDouble(double mensagem){
+       mensagem = scanner.nextDouble();
+       scanner.nextLine();
+       return mensagem;
+   }
+
+
 }
