56a185cad22f0b1178fcb496514f064fdc465390
José Lourenco <josern1771@gmail.com>
2026-07-24 23:23:24 -0300

    implementaçãode logica de listar contas

--- src/projetos/sistemabancario/src/operacoes/ListarConta.java
+++ src/projetos/sistemabancario/src/operacoes/ListarConta.java
@@ -4,6 +4,7 @@ import projetos.sistemabancario.src.banco.Banco;
 
 public class ListarConta {
     public void executar(Banco banco){
-
+        System.out.println("Listando contas: ");
+        banco.listarContas();
     }
 }
