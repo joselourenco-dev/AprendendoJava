e560110897a0ffa7bab0b932269e55ca6fbe4a4f
José Lourenco <josern1771@gmail.com>
2026-07-24 22:48:14 -0300

    Construindo logica de remover  conta

--- src/projetos/sistemabancario/src/operacoes/RemoverConta.java
+++ src/projetos/sistemabancario/src/operacoes/RemoverConta.java
@@ -5,7 +5,9 @@ import projetos.sistemabancario.src.banco.Banco;
 import java.util.Scanner;
 
 public class RemoverConta {
-    public void executar(Banco banco, Scanner entrada){
-
+    public void executar(Banco banco, Scanner entrada) {
+        System.out.println("Digite o numero da conta que deseja remover");
+        int numeroConta = entrada.nextInt();
+        banco.removerConta(numeroConta);
     }
 }
