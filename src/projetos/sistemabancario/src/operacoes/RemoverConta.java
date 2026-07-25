31573a95d337cf863e4056ae66e02c07059e19e2
José Lourenco <josern1771@gmail.com>
2026-07-24 23:56:16 -0300

    Correção de parametros do metodo

--- src/projetos/sistemabancario/src/operacoes/RemoverConta.java
+++ src/projetos/sistemabancario/src/operacoes/RemoverConta.java
@@ -1,13 +1,14 @@
 package projetos.sistemabancario.src.operacoes;
 
 import projetos.sistemabancario.src.banco.Banco;
+import projetos.sistemabancario.src.util.Entrada;
 
 import java.util.Scanner;
 
 public class RemoverConta {
-    public void executar(Banco banco, Scanner entrada) {
+    public void executar(Banco banco, Entrada entrada) {
         System.out.println("Digite o numero da conta que deseja remover");
-        int numeroConta = entrada.nextInt();
+        int numeroConta = entrada.lerInteiro();
         banco.removerConta(numeroConta);
     }
 }
