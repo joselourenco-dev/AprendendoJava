67126fec52a01bb05afa8ea15c1c4fb8732c8500
José Lourenco <josern1771@gmail.com>
2026-07-25 22:37:43 -0300

    Adição de opções

--- src/projetos/sistemabancario/src/util/Menu.java
+++ src/projetos/sistemabancario/src/util/Menu.java
@@ -5,6 +5,8 @@ public class Menu {
         System.out.println("=========================");
         System.out.println("1 - Criar conta");
         System.out.println("2 - Listar contas");
+        System.out.println("3 - Buscar Conta");
+        System.out.println("4 - Remover Conta");
         System.out.println("0 - Sair do menu");
         System.out.println("=========================");
     }
