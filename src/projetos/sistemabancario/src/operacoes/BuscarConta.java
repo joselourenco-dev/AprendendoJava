c52ed154a395c53af9f508690e8484233ff5c74c
José Lourenco <josern1771@gmail.com>
2026-07-25 23:21:30 -0300

    Rafatorando

--- src/projetos/sistemabancario/src/operacoes/BuscarConta.java
+++ src/projetos/sistemabancario/src/operacoes/BuscarConta.java
@@ -1,12 +1,13 @@
 package projetos.sistemabancario.src.operacoes;
 
 import projetos.sistemabancario.src.banco.Banco;
+import projetos.sistemabancario.src.model.ContaBancaria;
 import projetos.sistemabancario.src.util.Entrada;
 
 public class BuscarConta {
-    public void executar(Banco banco, Entrada entrada){
+    public ContaBancaria executar(Banco banco, Entrada entrada){
         System.out.println("Digite o numero da conta que deseja buscar: ");
         int numeroConta = entrada.lerInteiro();
-        banco.buscarConta(numeroConta);
+        return banco.buscarConta(numeroConta);
     }
 }
