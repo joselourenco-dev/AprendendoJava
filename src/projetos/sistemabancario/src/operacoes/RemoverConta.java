28de010f47e3918ffe895b76d69ff5d436993ff0
José Lourenco <josern1771@gmail.com>
2026-07-25 22:37:08 -0300

    Refatoramento

--- src/projetos/sistemabancario/src/operacoes/RemoverConta.java
+++ src/projetos/sistemabancario/src/operacoes/RemoverConta.java
@@ -3,8 +3,6 @@ package projetos.sistemabancario.src.operacoes;
 import projetos.sistemabancario.src.banco.Banco;
 import projetos.sistemabancario.src.util.Entrada;
 
-import java.util.Scanner;
-
 public class RemoverConta {
     public void executar(Banco banco, Entrada entrada) {
         System.out.println("Digite o numero da conta que deseja remover");
