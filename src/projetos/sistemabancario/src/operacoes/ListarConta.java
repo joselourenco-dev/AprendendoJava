7cc0a844b4df0289940b65f953e9b1c319b703f8
José Lourenco <josern1771@gmail.com>
2026-07-25 22:35:28 -0300

    Ajuste da logica de listagem de contas

--- src/projetos/sistemabancario/src/operacoes/ListarConta.java
+++ src/projetos/sistemabancario/src/operacoes/ListarConta.java
@@ -1,10 +1,14 @@
 package projetos.sistemabancario.src.operacoes;
-
 import projetos.sistemabancario.src.banco.Banco;
+import projetos.sistemabancario.src.model.ContaBancaria;
+import java.util.ArrayList;
 
 public class ListarConta {
     public void executar(Banco banco){
         System.out.println("Listando contas: ");
-        banco.listarContas();
+        ArrayList<ContaBancaria> contas = banco.listarContas();
+        for(ContaBancaria contaBancaria: contas){
+            System.out.println("Conta numero: " + contaBancaria.getNumeroConta());
+        }
     }
 }
