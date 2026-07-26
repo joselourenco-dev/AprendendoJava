d2b332bafe33f1e28a6c56db5c754cf0cfb60ad0
José Lourenco <josern1771@gmail.com>
2026-07-25 23:22:40 -0300

    Função depositar

--- src/projetos/sistemabancario/src/operacoes/Depositar.java
+++ src/projetos/sistemabancario/src/operacoes/Depositar.java
@@ -1,4 +1,12 @@
 package projetos.sistemabancario.src.operacoes;
 
+import projetos.sistemabancario.src.banco.Banco;
+import projetos.sistemabancario.src.util.Entrada;
+
 public class Depositar {
+    public void executar(Banco banco, Entrada entrada){
+        System.out.println("Valor do depósito: ");
+        double valor = entrada.lerDouble();
+        banco.Depositar(valor);
+    }
 }
