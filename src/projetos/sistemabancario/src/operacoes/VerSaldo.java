39a0f8ac49242264e20afe94af788e225c37cd05
José Lourenco <josern1771@gmail.com>
2026-07-26 22:58:27 -0300

    Criando funca de ver saldo

--- src/projetos/sistemabancario/src/operacoes/VerSaldo.java
+++ src/projetos/sistemabancario/src/operacoes/VerSaldo.java
@@ -1,4 +1,12 @@
 package projetos.sistemabancario.src.operacoes;
 
+import projetos.sistemabancario.src.model.ContaBancaria;
+import projetos.sistemabancario.src.util.Entrada;
+
 public class VerSaldo {
+    public void executar(ContaBancaria conta, Entrada entrada){
+        double saldo = conta.getSaldo();
+        System.out.println("saldo atual: " + saldo);
+    }
+
 }
