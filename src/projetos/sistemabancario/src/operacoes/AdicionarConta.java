de765a4089297f2041cbba1064e9b67260b81495
José Lourenco <josern1771@gmail.com>
2026-07-26 22:57:08 -0300

    notificação de conta adicionada

--- src/projetos/sistemabancario/src/operacoes/AdicionarConta.java
+++ src/projetos/sistemabancario/src/operacoes/AdicionarConta.java
@@ -4,10 +4,11 @@ import projetos.sistemabancario.src.banco.Banco;
 import projetos.sistemabancario.src.util.Entrada;
 
 public class AdicionarConta {
-      public void executar(Banco banco, Entrada  entrada){
+    public void executar(Banco banco, Entrada entrada) {
         System.out.print("Digite o nome do titular: ");
         String titular = entrada.lerTexto();
         banco.adicionarConta(titular);
+        System.out.println("Conta adicionada com sucesso");
     }
 }
 
