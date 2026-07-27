f85888d30fe5d92b2fcd68edb4f139a9a2af8319
José Lourenco <josern1771@gmail.com>
2026-07-26 21:25:15 -0300

    Correção de logica de remoção da conta

--- src/projetos/sistemabancario/src/operacoes/RemoverConta.java
+++ src/projetos/sistemabancario/src/operacoes/RemoverConta.java
@@ -1,12 +1,20 @@
 package projetos.sistemabancario.src.operacoes;
 
 import projetos.sistemabancario.src.banco.Banco;
+import projetos.sistemabancario.src.model.ContaBancaria;
 import projetos.sistemabancario.src.util.Entrada;
 
 public class RemoverConta {
     public void executar(Banco banco, Entrada entrada) {
         System.out.println("Digite o numero da conta que deseja remover");
         int numeroConta = entrada.lerInteiro();
-        banco.removerConta(numeroConta);
+
+       ContaBancaria conta =  banco.buscarConta(numeroConta);
+        if (conta == null){
+            System.out.println("Conta não existe");
+        }else {
+            banco.removerConta(numeroConta);
+        }
+
     }
 }
