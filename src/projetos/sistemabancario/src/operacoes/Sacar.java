c555e1e4883a236c7385dd894eb6c11328c23edf
José Lourenco <josern1771@gmail.com>
2026-07-26 22:58:07 -0300

    Criando funcao de saque

--- src/projetos/sistemabancario/src/operacoes/Sacar.java
+++ src/projetos/sistemabancario/src/operacoes/Sacar.java
@@ -1,4 +1,23 @@
 package projetos.sistemabancario.src.operacoes;
 
+import projetos.sistemabancario.src.exceptions.SaldoInsuficienteException;
+import projetos.sistemabancario.src.model.ContaBancaria;
+import projetos.sistemabancario.src.util.Entrada;
+
 public class Sacar {
+    public void executar(ContaBancaria conta, Entrada entrada)  {
+        System.out.println("Digite o valor que deseja sacar: ");
+        double valor = entrada.lerDouble();
+
+        try {
+            conta.sacar(valor);
+            System.out.println("Saque efetuado com sucesso");
+
+        } catch (SaldoInsuficienteException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
 }
