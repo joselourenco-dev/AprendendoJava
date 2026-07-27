cac94115cff8d506c6e13f9fd3ec571f82c75ddd
José Lourenco <josern1771@gmail.com>
2026-07-26 23:18:43 -0300

    removendo parametro não usado

--- src/projetos/sistemabancario/src/operacoes/VerSaldo.java
+++ src/projetos/sistemabancario/src/operacoes/VerSaldo.java
@@ -1,10 +1,9 @@
 package projetos.sistemabancario.src.operacoes;
 
 import projetos.sistemabancario.src.model.ContaBancaria;
-import projetos.sistemabancario.src.util.Entrada;
 
 public class VerSaldo {
-    public void executar(ContaBancaria conta, Entrada entrada){
+    public void executar(ContaBancaria conta){
         double saldo = conta.getSaldo();
         System.out.println("saldo atual: " + saldo);
     }
