e13ac7e93f94e4c7ae7c3beb43b1ef04e1760523
José Lourenco <josern1771@gmail.com>
2026-08-07 17:25:17 -0300

    Aula Filas

--- src/estruturaDeDadosEAlgoritmos/Aula02.java
+++ src/estruturaDeDadosEAlgoritmos/Aula02.java
@@ -1,4 +1,23 @@
 package estruturaDeDadosEAlgoritmos;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Aula02 {
+    public static void main(String[] args){
+        Deque<String> fila = new ArrayDeque<>();
+        fila.offer("Cliente 1");
+        fila.offer("Cliente 2");
+        fila.offer("Cliente 3");
+
+        System.out.println("Próximo cliente a ser atendido: " + fila.peek());
+
+        while (!fila.isEmpty()){
+            System.out.println("Atendendo: " + fila.poll());
+        }
+
+    }
+
+
+
 }
