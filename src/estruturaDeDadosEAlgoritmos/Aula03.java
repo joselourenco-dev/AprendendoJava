f7705dcd13cfda810788b016f94648c997106418
José Lourenco <josern1771@gmail.com>
2026-08-14 17:44:35 -0300

    LinkedList

--- src/estruturaDeDadosEAlgoritmos/Aula03.java
+++ src/estruturaDeDadosEAlgoritmos/Aula03.java
@@ -1,4 +1,29 @@
 package estruturaDeDadosEAlgoritmos;
 
 public class Aula03 {
+    public static void main(String[] args){
+        No ana = new No("Ana");
+        No bruno = new No("Bruno");
+        No carla = new No("Carla");
+
+        ana.proximo = bruno;
+        bruno.proximo = carla;
+        No atual = ana;
+
+        while (atual != null){
+            System.out.println(atual.valor);
+            atual = atual.proximo;
+        }
+
+    }
+
+    public static class No{
+        String valor;
+        No proximo;
+
+        No(String valor){
+            this.valor = valor;
+            this.proximo = null;
+        }
+    }
 }
