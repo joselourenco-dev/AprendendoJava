5c70b3723b8e14133ee7603854f0a212b4c63aa0
José Lourenco <josern1771@gmail.com>
2026-09-08 08:51:21 -0300

    Hashmap ex

--- src/estruturaDeDadosEAlgoritmos/exercicios/Ex04.java
+++ src/estruturaDeDadosEAlgoritmos/exercicios/Ex04.java
@@ -1,4 +1,20 @@
 package estruturaDeDadosEAlgoritmos.exercicios;
 
 public class Ex04 {
-}
+
+        public static void main(String[] args) {
+            String str1 = "A"; // Hash code = 65
+            String str2 = "Q"; // Hash code = 81
+
+            int hash1 = str1.hashCode();
+            int hash2 = str2.hashCode();
+
+            int mod1 = Math.floorMod(hash1, 16);
+            int mod2 = Math.floorMod(hash2, 16);
+
+            System.out.println("String '" + str1 + "' -> Hash: " + hash1 + " -> Mod 16: " + mod1);
+            System.out.println("String '" + str2 + "' -> Hash: " + hash2 + " -> Mod 16: " + mod2);
+            System.out.println("Colisão detectada? " + (mod1 == mod2));
+        }
+    }
+
