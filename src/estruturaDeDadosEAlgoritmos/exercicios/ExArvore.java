f53ef7c17b2e52ec05b54ac04106f21f88c37727
José Lourenco <josern1771@gmail.com>
2026-09-09 19:42:05 -0300

    exercicio

--- src/estruturaDeDadosEAlgoritmos/exercicios/ExArvore.java
+++ src/estruturaDeDadosEAlgoritmos/exercicios/ExArvore.java
@@ -1,4 +1,24 @@
 package estruturaDeDadosEAlgoritmos.exercicios;
 
+import estruturaDeDadosEAlgoritmos.ArvoreBinaria;
+import estruturaDeDadosEAlgoritmos.NoArvore;
+
 public class ExArvore {
+    public static void main(String[] args) {
+        ArvoreBinaria arvoreBinaria = new ArvoreBinaria();
+
+        arvoreBinaria.inserir(50);
+        arvoreBinaria.inserir(30);
+        arvoreBinaria.inserir(70);
+        arvoreBinaria.inserir(20);
+        arvoreBinaria.inserir(40);
+        arvoreBinaria.inserir(60);
+       arvoreBinaria.imprimirBusca(60);
+
+
+
+
+
+
+    }
 }
